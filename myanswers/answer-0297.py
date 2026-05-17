9ef616d1c16bf34bcc35a95230d371adf0c15845
VSofia-1 <sofia.evasquez01@gmail.com>
2026-05-17 13:16:39 -0500

    Update answer-0297.py

--- myanswers/answer-0297.py
+++ myanswers/answer-0297.py
@@ -33,37 +33,6 @@ def calcular_error_precio_casas(X, y):
 
     return float(mse)
 
-
-# ---------------------------------------------------------
-# CASO DE USO
-# ---------------------------------------------------------
-def generar_caso_de_uso_calcular_error_precio_casas():
-
-    n_muestras = np.random.randint(50, 200)
-    n_features = np.random.randint(2, 5)
-
-    X = np.random.rand(n_muestras, n_features) * 100
-
-    coeficientes = np.random.rand(n_features)
-
-    ruido = np.random.randn(n_muestras) * 10
-
-    y = X @ coeficientes + ruido
-
-    X_df = pd.DataFrame(X)
-
-    info = {
-        "descripcion":
-        "Cálculo de error en predicción de precios de casas",
-
-        "n_muestras": n_muestras,
-
-        "n_features": n_features
-    }
-
-    return info, X_df, y
-
-
 # ---------------------------------------------------------
 # EJECUCIÓN
 # ---------------------------------------------------------
