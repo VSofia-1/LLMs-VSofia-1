e4f81a7a8c70034b18fb2c4dfd6f1ec92f301115
VSofia-1 <sofia.evasquez01@gmail.com>
2026-05-17 12:41:39 -0500

    Add files via upload

---
+++ myanswers/answer-168.py
@@ -0,0 +1,26 @@
+import numpy as np
+import random
+from sklearn.metrics.pairwise import rbf_kernel
+
+# ---------------- FUNCIÓN PRINCIPAL ----------------
+
+def calcular_kernel_rbf(X, gamma):
+    kernel_matrix = rbf_kernel(X, gamma=gamma)    
+
+    return rbf_kernel(X, gamma=gamma)
+    
+# ---------------- PRUEBA ----------------
+
+input_case, output_case = generar_caso_de_uso_kernel()
+
+# usar tu función
+resultado = calcular_kernel_rbf(
+    input_case["X"],
+    input_case["gamma"]
+)
+
+print("INPUT:")
+print(input_case)
+
+print("\nOUTPUT")
+print(resultado)
