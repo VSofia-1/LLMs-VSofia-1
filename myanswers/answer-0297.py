c901f0cd1806fbc3b30431641a5c6311ee14b628
VSofia-1 <sofia.evasquez01@gmail.com>
2026-05-19 20:55:04 -0500

    Update answer-0297.py

--- myanswers/answer-0297.py
+++ myanswers/answer-0297.py
@@ -6,14 +6,7 @@ from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
 
 
-def calcular_error_precio_casas(X=None, y=None, **kwargs):
-
-    # Por si llegan dentro de kwargs
-    if X is None:
-        X = kwargs.get("X")
-
-    if y is None:
-        y = kwargs.get("y")
+def calcular_error_precio_casas(info, X, y):
 
     # División entrenamiento/prueba
     X_train, X_test, y_train, y_test = train_test_split(
@@ -36,3 +29,7 @@ def calcular_error_precio_casas(X=None, y=None, **kwargs):
     mse = mean_squared_error(y_test, y_pred)
 
     return float(mse)
+    
+    info, X, y = generar_caso_de_uso_calcular_error_precio_casas()
+
+    resultado = calcular_error_precio_casas(info, X, y)
